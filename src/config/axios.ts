8ed66a77cb3c872170d145792c268b6497b8a1ea
linhngocdao <linhdaongoc22@gmail.com>
2025-01-22 14:43:06 +0700

    update

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -4,7 +4,8 @@ const axiosInstance: AxiosInstance = axios.create({
   timeout: 3 * 60 * 1000,
   // baseURL: "http://localhost:3000/api/movies",
   // baseURL: "http://103.82.194.253:3000/api/movies",
-  baseURL: "http://ophimvip.ddns.net:3000/api/movies",
+  // baseURL: "http://ophimvip.ddns.net:3000/api/movies",
+  baseURL: "https://dnling.id.vn/api/movies",
 });
 
 export default axiosInstance;
